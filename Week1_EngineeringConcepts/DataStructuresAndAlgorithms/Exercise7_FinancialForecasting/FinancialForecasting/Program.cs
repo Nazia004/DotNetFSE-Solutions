f7034dd3277f79a20b5895f9a26533fe534252a7
nazia-quadri <naziaquadri67@gmail.com>
2026-07-04 03:20:38 +0530

    Completed Exercise 7 - Financial Forecasting

--- Week1_EngineeringConcepts/DataStructuresAndAlgorithms/Exercise7_FinancialForecasting/FinancialForecasting/Program.cs
+++ Week1_EngineeringConcepts/DataStructuresAndAlgorithms/Exercise7_FinancialForecasting/FinancialForecasting/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FinancialForecasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double currentValue = 10000;
+            double growthRate = 0.10; // 10%
+            int years = 5;
+
+            double futureValue =
+                ForecastCalculator.PredictFutureValue(currentValue, growthRate, years);
+
+            Console.WriteLine("Financial Forecast");
+
+            Console.WriteLine($"Current Value : ₹{currentValue}");
+
+            Console.WriteLine($"Growth Rate   : {growthRate * 100}%");
+
+            Console.WriteLine($"Years         : {years}");
+
+            Console.WriteLine($"Future Value  : ₹{futureValue:F2}");
+        }
+    }
+}
