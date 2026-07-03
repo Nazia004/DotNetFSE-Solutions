169679985262eab1984a345ae4ffaf30107a06d2
nazia-quadri <naziaquadri67@gmail.com>
2026-07-04 00:09:24 +0530

    Completed Singleton Pattern

---
+++ Week1_EngineeringConcepts/DesignPatterns/SingletonPatternExample/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SingletonPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger1 = Logger.GetInstance();
+            Logger logger2 = Logger.GetInstance();
+
+            logger1.Log("First Log Message");
+            logger2.Log("Second Log Message");
+
+            if (logger1 == logger2)
+            {
+                Console.WriteLine("Both logger instances are the same.");
+            }
+            else
+            {
+                Console.WriteLine("Different instances.");
+            }
+        }
+    }
+}
