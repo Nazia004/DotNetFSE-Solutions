e205f13a7e80bd77bdce98611ede8c17be9337d2
nazia-quadri <naziaquadri67@gmail.com>
2026-07-28 22:24:16 +0530

    Completed Week 7 Angular Hands-on 7 Angular Dependency Injection

--- Week7_AngularAndReact/Angular/student-course-portal/src/app/components/home/home.ts
+++ Week7_AngularAndReact/Angular/student-course-portal/src/app/components/home/home.ts
@@ -4,7 +4,7 @@ import { CourseList } from '../course-list/course-list';
 import { StudentProfile } from '../student-profile/student-profile';
 import { CourseDashboard } from '../course-dashboard/course-dashboard';
 import { CoursePipes } from '../course-pipes/course-pipes';
-import { CourseService } from '../../services/course.service';
+import { CourseService } from '../../services/course.service'; 
 
 @Component({
   selector: 'app-home',
@@ -31,16 +31,10 @@ export class Home implements OnInit, OnDestroy {
 
   courses: string[] = [];
 
-  constructor(private courseService: CourseService) {
-  console.log('Constructor called');
-}
+  constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
-
   this.courses = this.courseService.getCourses();
-
-  console.log('HomeComponent Initialized');
-
 }
 
   ngOnDestroy(): void {
